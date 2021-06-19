2d0dad4b546dd301b3e0088a3793956070eef157
Esen Yolcu <esenyolcu1986@gmail.com>
2021-06-18 22:00:50 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI":"mongodb://localhost/contacts_list"
+    //"URI":"mongodb://localhost/contacts_list"
+    "URI":"mongodb+srv://esen:esen4821@mongodbserver.rwzmz.mongodb.net/contacts_list?retryWrites=true&w=majority"
 }
